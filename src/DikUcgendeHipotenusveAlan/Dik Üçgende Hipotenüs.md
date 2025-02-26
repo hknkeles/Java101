bcb76ee62df62ed7d339df7c546a5154633d844d
Hakan Keles <hakankkeles@gmail.com>
2025-02-26 12:45:41 +0300

    Ucgenin Alanı ve Hipotenusu Hesaplama

---
+++ src/DikUcgendeHipotenusveAlan/Dik Üçgende Hipotenüs.md
@@ -0,0 +1,18 @@
+# Dik Üçgende Hipotenüs Bulan Program
+
+Java ile kullanıcıdan dik kenarlarının uzunluğunu alan ve hipotenüsü hesaplayan programı yazın.
+
+(a\*a) + (b\*b) = c*c
+
+---
+Üç kenar uzunluğunu kullanıcıdan aldığınız üçgenin alanını hesaplayan programı yazınız.
+
+**Formül
+Üçgenin Çevresi**
+
+u = (a+b+c) / 2
+
+**Alan**
+$$
+\sqrt{u×(u−a)×(u−b)×(u−c)}
+$$
---
+++ src/DikUcgendeHipotenusveAlan/HipotenusHesaplama.java
@@ -0,0 +1,23 @@
+package DikUcgendeHipotenusveAlan;
+
+import java.util.Scanner;
+
+public class HipotenusHesaplama {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int kenar1, kenar2;
+        double kenar3;
+
+        System.out.println("Lütfen dik kenarların uzunluklarını giriniz.");
+        System.out.println("Birinci kenar uzunluğu: ");
+        kenar1 = scanner.nextInt();
+        System.out.println("İkinci kenar uzunluğu: ");
+        kenar2 = scanner.nextInt();
+
+        kenar3 = Math.sqrt((kenar1 * kenar1) + (kenar2 * kenar2));
+
+        System.out.printf("Hipotenüs: %.2f", kenar3);
+    }
+}
